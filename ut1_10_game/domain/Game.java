c0e6e0cb4ee2e8d52d9a7364ec9df2b20231f786
Antonio F Pelegrín <37382256+apelegrin@users.noreply.github.com>
2023-11-10 10:46:06 +0100

    Add files via upload

--- ut1_10_game/domain/Game.java
+++ ut1_10_game/domain/Game.java
@@ -8,16 +8,35 @@ public class Game {
     private int alto;
     private int lado;
     private int redibujar;
+    private boolean finJuego;
+    private Pantalla pantalla;
     
 	public Game(int ancho, int alto, int lado, int redibujar) {
 		this.ancho = ancho;
 		this.alto = alto;
 		this.lado = lado;
 		this.redibujar = redibujar ;
+		this.finJuego = false;
 	}
 
 	public void iniciar() {
-		// TODO Auto-generated method stub
+		String rutaImagenFondo ="imagenes/panel-basico.gif";
+        pantalla = new Pantalla(ancho,alto,lado,Color.BLUE);
+		while (!finJuego) {
+			pantalla.resetear();
+			//Recorrer todos los objetos de pantalla y agregar su imagen
+			cargarFondo(rutaImagenFondo);
+			pantalla.dibujar();
+			Alarma.dormir(redibujar);
+		}
+	}
+	
+	private void cargarFondo(String rutaImagenFondo) {
+		for (int i = 0; i < this.ancho; i++) {
+			for (int j = 0; j < this.alto; j++) {
+			    pantalla.addImagen(i, j, rutaImagenFondo);
+			}
+		}
 		
 	}
 
