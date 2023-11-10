9af017304cbdb91b917a0a12a31442ebac01a3d4
Antonio F Pelegrín <37382256+apelegrin@users.noreply.github.com>
2023-11-10 10:29:52 +0100

    Add files via upload

---
+++ ut1_10_game/domain/Game.java
@@ -0,0 +1,24 @@
+package domain;
+
+import topos.vista1.*;
+import java.awt.Color;
+
+public class Game {
+    private int ancho;
+    private int alto;
+    private int lado;
+    private int redibujar;
+    
+	public Game(int ancho, int alto, int lado, int redibujar) {
+		this.ancho = ancho;
+		this.alto = alto;
+		this.lado = lado;
+		this.redibujar = redibujar ;
+	}
+
+	public void iniciar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
